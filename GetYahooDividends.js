d9359c5201ccbf4b87b19a5ac176bedf8d95967d
Jerry Vigil <jerry_vigil@cable.comcast.com>
2013-02-26 13:56:14 -0800

    1:56 PM commit

--- GetYahooDividends.js
+++ GetYahooDividends.js
@@ -21,13 +21,13 @@ function pushRows(ticker,respBody) {
       if ( vals[0] > oneYearAgoISO ) {
           row.ttmd += (vals[1]*1.0);
       }
-      callback();
   }, function(){
-     rows.push( row );
+     //rows.push( row );
+     return row;
   });
 }
 
-function getYahoo(ticker,callback) {
+var getYahooHistory = function(ticker) {
  var prefix = 'http://ichart.finance.yahoo.com/table.csv?s=';
  var suffix = '&a='+today.getMonth()+'&b='+today.getDate()+'&c='+(today.getFullYear()-1)+'&d='+today.getMonth()+'&e='+today.getDate()+'&f='+today.getFullYear()+'&g=v&ignore=.csv';
  
@@ -37,20 +37,56 @@ function getYahoo(ticker,callback) {
       respBody += chunk;
    });
    resp.on('end',function(){
-      pushRows(ticker,respBody.toString());
-      callback();
+      var row = pushRows(ticker,respBody.toString());
+      return row;
+      //callback(ticker,respBody.toString());
    });
  }).on('error',function(e){
-   console.log('ERROR = '+e.message);
+   console.log('ERROR HISTORY = '+e.message);
  });
+};
+
+var getYahooLast = function(ticker){
+	var prefix = 'http://download.finance.yahoo.com/d/quotes.csv?s=';
+	var suffix = '&f=sl1'
+	console.log( "Ticker = " + ticker );
+		
+	http.get(prefix+ticker+suffix,function(resp){
+		var respBody = '';
+		resp.on('data',function(chunk){
+		      respBody += chunk;
+		});
+		resp.on('end',function(){
+		  var cols = respBody.split(',');
+		  return cols[1];
+		});
+	}).on('error',function(e){
+		console.log('ERROR LAST = '+e.message);
+	});	
+};
+
+function getYahoos(ticker,callback){ 
+	async.parallel([function(callback){ 
+						var result = getYahooHistory(ticker);
+						callback(null,result);
+				    },
+				    function(callback){ 
+				    	var result = getYahooLast(ticker);
+				    	callback(null,result);
+				    }],
+					function(err,results) {
+						console.log('Ticker = '+ticker+' done.');
+						console.log(results);
+					});
+	callback();
 }
 
-// var tickers = new Array('rem','kbwd','hyld','dwx','jnk','hyg','amj','amlp','pcef','pgf','pgx','pff');
-var tickers = ['nly','agnc','hts','ivr','cim','bpt','kmp'];
+var tickers = new Array('rem','kbwd','hyld','dwx','jnk','hyg','amj','amlp','pcef','pgf','pgx','pff');
+//var tickers = ['nly','agnc','hts','ivr','cim','bpt','kmp','eep'];
 
 //This just gives you the values sorted by the gross value of the dividend, and not by the dividend yield.
-async.forEach( tickers, getYahoo, function(){
-    rows.sort( function(a,b){ return b.ttmd - a.ttmd; } );
-    console.log( rows );
-});
 
+async.each( tickers, getYahoos, function(){
+    //rows.sort( function(a,b){ return b.ttmd - a.ttmd; } );
+    //console.log( rows );
+});
